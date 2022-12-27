f6a95f0a30febde864eba9cc94bc768736743b27
alejandrolamela <alejandro.lamela@bootcamp-upgrade.com>
2022-12-27 18:26:14 +0100

    second

--- pokedex.js
+++ pokedex.js
@@ -1,12 +1,67 @@
-const listPokedex = document.querySelector("#pokedex")
+const pokemonContainer = document.querySelector('#pokedex');
 
-function getPokemons(){
+function fetchPokemon(id) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  .then( response  => response.json() )
+  .then( data => {
+    createPokemon(data);
+    // console.log(data);
+  })
+}
 
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
-    .then(function(response) { // sacar de la bolsa
-        return response.json();
-      })
+function pokemon(number) {
+  for (let i = 1; i < number; i++) {
+    fetchPokemon(i);
+    
+  }
+}
 
-};
 
-getPokemons();
+
+
+
+
+function createPokemon(pokemon) {
+  const card = document.createElement('div');
+  card.classList.add('card-container');
+
+  const spriteContainer = document.createElement('div');
+  spriteContainer.classList.add('image-container');
+
+  const sprite = document.createElement('img');
+  sprite.src = pokemon.sprites.front_default;
+
+  spriteContainer.appendChild(sprite);
+
+  const numberId = document.createElement('p');
+  numberId.textContent = pokemon.id;
+
+  const namePokemon = document.createElement('p');
+  namePokemon.classList.add('name');
+  namePokemon.textContent = pokemon.name;
+
+  const weightPokemon = document.createElement('p');
+  weightPokemon.textContent= pokemon.weight;
+
+
+  const typesPokemon = document.createElement('p');
+// Aqui cree un array vacio para meter cada uno de los tipos de cada pokemon
+  let arrayTypesPokemon=[];
+  for (let i = 0; i < pokemon.types.length; i++) {
+    arrayTypesPokemon.push(pokemon.types[i].type.name);
+    
+  }
+    typesPokemon.textContent = arrayTypesPokemon;
+    
+ 
+  card.appendChild(spriteContainer);
+  
+  card.appendChild(numberId);
+  card.appendChild(namePokemon);
+  card.appendChild(weightPokemon);
+  card.appendChild(typesPokemon);
+   pokemonContainer.appendChild(card);
+}
+
+pokemon(150);
+
